a828baa35c52f741e56c145d4a533d2d3d81f5e2
ogakulov <ogakulov@gmail.com>
2023-01-22 01:12:55 -0800

    Comments for mlflow experiments
    Removed comments covering legacy Experiments and updated comments for mlflow tracking

--- code/4_train_models.py
+++ code/4_train_models.py
@@ -66,59 +66,14 @@
 # The rest can be left as is. Once the job has been created, click **Run** to start a manual
 # run for that job.
 
-# ***Experiments***
+# ***Experiments with mlflow***
 #
 # Training a model for use in production requires testing many combinations of model parameters
 # and picking the best one based on one or more metrics.
-# In order to do this in a *principled*, *reproducible* way, an Experiment executes model training code with **versioning** of the **project code**, **input parameters**, and **output artifacts**.
+# In order to do this in a *principled*, *reproducible* way, Experiments (based on mlflow tracking) record **input parameters**, and **output artifacts**.
 # This is a very useful feature for testing a large number of hyperparameters in parallel on elastic cloud resources.
 
-# **[Experiments](https://docs.cloudera.com/machine-learning/cloud/experiments/topics/ml-running-an-experiment.html)**.
-# run immediately and are used for testing different parameters in a model training process.
-# In this instance it would be use for hyperparameter optimisation. To run an experiment, from the
-# Project window click Experiments > Run Experiment with the following settings.
-# * **Script** : 4_train_models.py
-# * **Arguments** : 5 lbfgs 100 _(these the cv, solver and max_iter parameters to be passed to
-# LogisticRegressionCV() function)
-# * **Kernel** : Python 3
-# * **Engine Profile** : 1 vCPU / 2 GiB
-
-# Click **Start Run** and the expriment will be sheduled to build and run. Once the Run is
-# completed you can view the outputs that are tracked with the experiment using the
-# `cdsw.track_metrics` function. It's worth reading through the code to get a sense of what
-# all is going on.
-
-# More Details on Running Experiments
-# Requirements
-# Experiments have a few requirements:
-# - model training code in a `.py` script, not a notebook
-# - `requirements.txt` file listing package dependencies
-# - a `cdsw-build.sh` script containing code to install all dependencies
-#
-# These three components are provided for the churn model as `4_train_models.py`, `requirements.txt`,
-# and `cdsw-build.sh`, respectively.
-# You can see that `cdsw-build.sh` simply installs packages from `requirements.txt`.
-# The code in `4_train_models.py` is largely identical to the code in the last notebook.
-# with a few differences.
-#
-# The first difference from the last notebook is at the "Experiments options" section.
-# When you set up a new Experiment, you can enter
-# [**command line arguments**](https://docs.python.org/3/library/sys.html#sys.argv)
-# in standard Python fashion.
-# This will be where you enter the combination of model hyperparameters that you wish to test.
-#
-# The other difference is at the end of the script.
-# Here, the `cdsw` package (available by default) provides
-# [two methods](https://docs.cloudera.com/machine-learning/cloud/experiments/topics/ml-tracking-metrics.html)
-# to let the user evaluate results.
-#
-# **`cdsw.track_metric`** stores a single value which can be viewed in the Experiments UI.
-# Here we store two metrics and the filepath to the saved model.
-#
-# **`cdsw.track_file`** stores a file for later inspection.
-# Here we store the saved model, but we could also have saved a report csv, plot, or any other
-# output file.
-#
+# In this instance it would be used for hyperparameter optimisation. To learn more about mlflow tracking, visit https://mlflow.org/docs/latest/tracking.html
 
 
 from pyspark.sql.types import *
@@ -201,40 +156,14 @@ ct = ColumnTransformer(
     remainder="passthrough"
 )
 
-## Experiments options
-## If you are running this as an experiment, pass the cv, solver and max_iter values
-## as arguments in that order. e.g. `5 lbfgs 100`.
-#if len(sys.argv) == 4:
-#    try:
-#        cv = int(sys.argv[1])
-#        solver = str(sys.argv[2])
-#        max_iter = int(sys.argv[3])
-#    except:
-#        sys.exit("Invalid Arguments passed to Experiment")
-#else:
-#    cv = 5
-#    solver = "lbfgs"  # one of newton-cg, lbfgs, liblinear, sag, saga
-#    max_iter = 100
-#
-## Instantiate the model
-#clf = LogisticRegressionCV(cv=cv, solver=solver, max_iter=max_iter)
-#pipe = Pipeline([("ct", ct), ("scaler", StandardScaler()), ("clf", clf)])
-#
-## Train the model
-#pipe.fit(X_train, y_train)
-#
-## Capture train and test set scores
-#train_score = pipe.score(X_train, y_train)
-#test_score = pipe.score(X_test, y_test)
-#print("train", train_score)
-#print("test", test_score)
-#print(classification_report(y_test, pipe.predict(X_test)))
-#datadf[labels.name + " probability"] = pipe.predict_proba(X)[:, 1]
-
+# Instantiate a new set of experiments (mlflow experiment object)
 mlflow.set_experiment("Churn Model Tuning")
+
+# Define a search grid
 kernel = ["linear", "rbf"]
 max_iter = [1, 10, 100, 1000, 10000]
 
+# Iterate over the grid, re-training the model every time and recording train and test score as the metrics
 for k in kernel:
   for i in max_iter:
     
@@ -251,9 +180,6 @@ for k in kernel:
     # Capture train and test set scores
     train_score2 = svc_pipe.score(X_train, y_train)
     test_score2 = svc_pipe.score(X_test, y_test)
-    #print("train", train_score2)
-    #print("test", test_score2)
-    #print(classification_report(y_test, svc_pipe.predict(X_test)))
     datadf[labels.name + " probability"] = svc_pipe.predict_proba(X)[:, 1]
     
     mlflow.log_metric("train_score", round(train_score2, 2))
@@ -284,22 +210,3 @@ explainedmodel = ExplainedModel(
     explainer=explainer,
 )
 explainedmodel.save(model_name='telco_linear')
-
-
-# If running as as experiment, this will track the metrics and add the model trained in this
-# training run to the experiment history.
-#cdsw.track_metric("train_score", round(train_score, 2))
-#cdsw.track_metric("test_score", round(test_score, 2))
-#cdsw.track_metric("model_path", explainedmodel.model_path)
-#cdsw.track_file(explainedmodel.model_path)
-
-# Wrap up
-
-# We've now covered all the steps to **running Experiments**.
-#
-# Notice also that any script that will run as an Experiment can also be run as a Job or in a Session.
-# Our provided script can be run with the same settings as for Experiments.
-# A common use case is to **automate periodic model updates**.
-# Jobs can be scheduled to run the same model training script once a week using the latest data.
-# Another Job dependent on the first one can update the model parameters being used in production
-# if model metrics are favorable.
